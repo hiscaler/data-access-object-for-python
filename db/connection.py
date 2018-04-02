342515a85719492f84136273ab443b9ccc0432a8
hiscaler <hiscaler@gmail.com>
2018-04-02 22:27:32 +0800

    Prefection connection class.

--- db/connection.py
+++ db/connection.py
@@ -1,19 +1,31 @@
 # encoding=utf-8
 
+"""
+see http://kuanghy.github.io/2015/08/16/python-dbapi
+"""
+from dbexceptions import DatabaseErrorException
+
 
 class Connection(object):
-
+    apilevel = '1.0'
+    paramstyle = 'named'
+    threadsafety = 3
+    name = None
     schemaMap = {
         'mysql': 'db/mysql/schema'
     }
+    _drivers_map = {
+        'pymysql': 'pymysql',
+    }
 
-    def __init__(self, config={}):
+    def __init__(self, driver, config={}):
+        self._driver = driver.lower()
         self._db_class = ''
-        self._db = None
+        self.db = None
         dsn = config['dsn'] if 'dsn' in config else None
         username = config['username'] if 'username' in config else ''
         password = config['password'] if 'password' in config else ''
-        if dsn is None or not username or not password:
+        if not username or not password:
             raise Exception('Error DSN or username or password value.')
 
         self.dsn = dsn
@@ -24,16 +36,29 @@ class Connection(object):
         self.port = 3306
         self.charset = 'utf-8'
         self.table_prefix = ''
+        self.cursor = None
 
-    def create_db_instance(self):
-        pass
+    def connection(self):
+        if self.db is None:
+            if self._driver is not None and self._driver in self._drivers_map:
+                driver_class = self._drivers_map[self._driver]
+                try:
+                    __import__(driver_class, globals=globals())
+                    print('Driver class is ' + driver_class)
+                    if driver_class == 'pymysql':
+                        import pymysql
+                        self.db = pymysql.connect(self.host, self.username, self.password, self.database)
+                except ImportError as ex:
+                    print("Import error, {message}.".format(message=str(ex)))
 
-    def open(self):
-        if self._db is None:
-            return self.create_db_instance()
+            else:
+                raise DatabaseErrorException(
+                    '{driver} is not supported or driver name is error.'.format(driver=self._driver))
+
+        return self.db;
 
     def close(self):
-        self._db = None
+        self.db = None
 
     def commit(self):
         pass
@@ -42,8 +67,19 @@ class Connection(object):
         pass
 
     def cursor(self):
-        pass
+        if self.db is not None:
+            return self.db.cursor()
+        else:
+            raise DatabaseErrorException('No active connection.')
 
 
 if __name__ == '__main__':
-    db = Connection()
+    conn = Connection('pymysql', {
+        'username': 'root',
+        'password': 'root',
+        'database': 'dao_test',
+        'charset': 'utf-8',
+        'table_prefix': '',
+    })
+    db = conn.connection()
+    print(db)
