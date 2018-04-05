9481679cea744911086097e8c7581f005ec924ae
hiscaler <hiscaler@gmail.com>
2018-04-05 23:41:39 +0800

    ...

--- db/schema.py
+++ db/schema.py
@@ -1,31 +1,133 @@
 # encoding=utf-8
 
+from db.dbexceptions import DatabaseErrorException
+from db.dbexceptions import NotSupportedErrorException
 from db.query_builder import QueryBuilder
 
 
 class Schema(object):
     TYPE_PK = 'pk'
     TYPE_UPK = 'upk'
-    TYPE_GIGPK = 'bigpk',
-    TYPE_UGIGPK = 'ubigpk'
+    TYPE_BIGPK = 'bigpk'
+    TYPE_UBIGPK = 'ubigpk'
     TYPE_CHAR = 'char'
     TYPE_STRING = 'string'
     TYPE_TEXT = 'text'
     TYPE_SMALLINT = 'smallint'
     TYPE_INTEGER = 'integer'
+    TYPE_BIGINT = 'bigint'
+    TYPE_FLOAT = 'float'
+    TYPE_DOUBLE = 'double'
+    TYPE_DECIMAL = 'decimal'
+    TYPE_DATETIME = 'datetime'
+    TYPE_TIMESTAMP = 'timestamp'
+    TYPE_TIME = 'time'
+    TYPE_DATE = 'date'
+    TYPE_BINARY = 'binary'
+    TYPE_BOOLEAN = 'boolean'
+    TYPE_MONEY = 'money'
 
     _table_quote_character = "'"
 
     _column_quote_character = '"'
 
-
     def __init__(self, db):
         self.db = db
         self.default_schema = ''
         self._table_names = set()
-        self._tables = set()
+        self._schema_names = None
+        self._tables = {}
         self._builder = None
         pass
+
+    def load_table_schema(self, name):
+        pass
+
+    def get_table_schema(self, name, refresh=False):
+        if name in self._tables and not refresh:
+            return self._tables[name]
+
+        db = self.db
+        real_name = self.get_raw_table_name(name)
+        self._tables[name] = self.load_table_schema(real_name)
+
+        return self._tables[name]
+
+    def get_table_schemas(self, schema='', refresh=False):
+        tables = set()
+        for name in self.get_table_names(schema, refresh):
+            if schema != '':
+                name = schema + '.' + name
+            table = self.get_table_schema(name, refresh)
+            if table is not None:
+                tables.add(table)
+
+        return tables
+
+    def get_schema_names(self, refresh=False):
+        if self._schema_names is None or refresh:
+            self._schema_names = self.find_schema_names()
+
+        return self._schema_names
+
+    def get_table_names(self, schema, refresh=False):
+        if schema not in self._table_names or refresh:
+            self._table_names[schema] = self.find_table_names(schema)
+
+        return self._table_names[schema]
+
+    def get_query_builder(self):
+        if self._builder is None:
+            self._builder = self.create_query_builder()
+
+        return self._builder
+
+    def get_type(self, data):
+        pass
+
+    def refresh(self):
+        pass
+
+    def create_query_builder(self):
+        return QueryBuilder(self.db)
+
+    def find_schema_names(self):
+        raise NotSupportedErrorException(self.__class__.__name__ + ' does not support fetching all schema names.')
+
+    def find_table_names(self):
+        raise NotSupportedErrorException(self.__class__.__name__ + ' does not support fetching all table names.')
+
+    def get_last_insert_id(self, sequence_name=''):
+        if self.db.is_active:
+            return self.db.cursor.last_insert_id(None if sequence_name == '' else self.quote_table_name(sequence_name))
+        else:
+            raise DatabaseErrorException('DB Connection is not active.')
+
+    def supports_savepoint(self):
+        return self.db.enable_savepoint
+
+    def create_savepoint(self, name):
+        self.db.create_command('SAVEPOINT' + name).execute()
+
+    def release_savepoint(self, name):
+        self.db.create_command('RELEASE SAVEPOINT ' + name).execute()
+
+    def rollback_savepoint(self, name):
+        self.db.create_command('ROLLBACK TO SAVEPOINT ' + name).execute()
+
+    def set_transaction_isolation_level(self, level):
+        self.db.create_command('SET TRANSACTION ISOLATION LEVEL ' + level).execute()
+
+    def insert(self, table, columns):
+        command = self.db.create_command().insert(table, columns)
+        if not command.execute():
+            return False
+        result = {}
+        table_schema = self.get_table_schema(table)
+        for name in table_schema.primary_key:
+            pass
+
+        return result
 
     def quote_value(self, value):
         if not isinstance(value, str):
@@ -104,12 +206,3 @@ class Schema(object):
             name = name
 
         return name
-
-    def get_query_builder(self):
-        if self._builder is None:
-            self._builder = self.create_query_builder()
-
-        return self._builder
-
-    def create_query_builder(self):
-        return QueryBuilder(self.db)
